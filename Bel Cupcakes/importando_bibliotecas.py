6ec501ba0a199cd777fc02101bff9e4d47f60726
Bruno Kiyota Gomes de Oliveira <bruno.kiyota@gmail.com>
2021-05-13 19:10:46 -0300

    Adição de Projetos

---
+++ Bel Cupcakes/importando_bibliotecas.py
@@ -0,0 +1,11 @@
+def importa_bibliotecas_ds():
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+
+    import warnings
+    warnings.filterwarnings('ignore')
+    
+    return print("bibliotecas importadas")
+  
